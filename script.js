14dc8c392ea5240c8c0e586556458041c3addc83
Fezin <126676831+Ftfezin@users.noreply.github.com>
2024-02-02 20:09:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal ({
+    distance: '90px' , 
+    duration: 2400,
+    reset: true
+});
+
+sr.reveal('.logo', {delay:110, origin: "left"});
+sr.reveal('.nav-bar', {delay:110, origin: "left"});
+sr.reveal('.btn', {delayy:110, origin: "top"});
+sr.reveal('.btn-box', {delay:110, origin: "top"});
+sr.reveal('.home-content h1', {delay:110, origin: "left"});
+sr.reveal('.home-content p', {delay:110, origin: "left"});
